3e08697d09fb84b4a4862b2df62b2f84c6204a86
Abhishek Badge <abhishekbadge023@gmail.com>
2025-11-20 23:13:56 +0530

    Remove unwanted models and routes folders

--- models/category.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const CategorySchema = new mongoose.Schema({
-    CategoryName: { type: String, required: true, unique: true }
-});
-
-module.exports = mongoose.model("Category", CategorySchema);
